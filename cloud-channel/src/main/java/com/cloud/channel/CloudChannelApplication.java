0df9ae0494c4afe2d7f384049eb70d0805dfdb2d
LPF <lipengfei@xcxd.com>
2021-01-04 17:01:09 +0800

    '项目初始化'

---
+++ cloud-channel/src/main/java/com/cloud/channel/CloudChannelApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.channel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudChannelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudChannelApplication.class, args);
+    }
+
+}
---
+++ cloud-question/src/main/java/com/cloud/question/CloudQuestionApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.question;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudQuestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudQuestionApplication.class, args);
+    }
+
+}
